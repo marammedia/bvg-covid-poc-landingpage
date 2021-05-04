c29ca607407807b99a4286f450934575a32b7011
MaraM.media <52410644+marammedia@users.noreply.github.com>
2021-05-04 20:45:05 +0200

    Kennzeichnung Barrierefreiheit

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+$config = array(
+  //'csv-url' => dirname(dirname(__FILE__)).'/corona/corona-schnelltest-termin-data.csv',
+  //'accessibility-url' => dirname(dirname(__FILE__)).'/corona/corona-schnelltest-barrierefreiheit-data.csv',
+  'csv-url' => 'https://beta.bvg.de/dam/jcr:2f4f829a-780b-4749-8207-e34c898f7704/corona-schnelltest-termin-data.csv',
+  'accessibility-url' => 'https://beta.bvg.de/dam/jcr:1260d759-fa07-4e0a-90be-9f7b6207cb4c/corona-schnelltest-barrierefreiheit-data.csv',
+);
--- index.php
+++ index.php
@@ -1,8 +1,55 @@
 <?php
 
-$config = array(
-  'csv-url' => 'https://beta.bvg.de/dam/jcr:2f4f829a-780b-4749-8207-e34c898f7704/corona-schnelltest-termin-data.csv',
-);
+$root = dirname(__FILE__);
+require_once $root.'/config.php';
+
+$timezone = new DateTimeZone('Europe/Berlin');
+
+$today = new DateTime();
+$today->setTime(0, 0, 0);
+$today->setTimezone($timezone);
+
+$data = array();
+$raw = @file_get_contents($config['csv-url']);
+$lines = explode("\n", $raw);
+foreach ($lines as $line) {
+  if (empty($line)) {
+    continue;
+  }
+
+  $line = html_entity_decode($line);
+  $rows = explode(';', $line, 6);
+  $rows = array_map('trim', $rows);
+
+  list($date, $location, $url, $hints, $begin, $end) = $rows;
+  $data[$date][$location] = array(
+    'url' => $url,
+    'hints' => $hints,
+    'startTime' => $begin,
+    'endTime' => $end,
+  );
+}
+ksort($data);
+
+
+$accessibilities = array();
+$raw = @file_get_contents($config['accessibility-url']);
+$lines = explode("\n", $raw);
+foreach ($lines as $line) {
+  if (empty($line)) {
+    continue;
+  }
+
+  $line = html_entity_decode($line);
+  $rows = explode(';', $line);
+  $rows = array_map('trim', $rows);
+
+  $location = array_shift($rows);
+
+  foreach ($rows as $row) {
+    $accessibilities[$location][] = explode('+', $row, 2);
+  }
+}
 
 ?>
 <!doctype html>
@@ -17,6 +64,8 @@ $config = array(
     <link rel="stylesheet" href="./css/style.css">
   </head>
   <body>
+
+
 
     <header>
       <div class="black-line">
@@ -46,34 +95,8 @@ $config = array(
       </section>
 
       <section>
-          <?php
+          <?php          
 
-          $timezone = new DateTimeZone('Europe/Berlin');
-
-          $today = new DateTime();
-          $today->setTime(0, 0, 0);
-          $today->setTimezone($timezone);
-
-          $data = array();
-
-          $raw = @file_get_contents($config['csv-url']);
-          $lines = explode("\n", $raw);
-          foreach ($lines as $line) {
-            if (empty($line)) {
-              continue;
-            }
-
-            $line = html_entity_decode($line);
-            list($date, $location, $url, $hints, $begin, $end) = explode(';', $line, 6);
-            $data[$date][$location] = array(
-              'url' => trim($url),
-              'hints' => trim($hints),
-              'startTime' => trim($begin),
-              'endTime' => trim($end),
-            );
-          }
-
-          ksort($data);
           foreach ($data as $date => $locations) {
             $datetime = new DateTime($date);
             $datetime->setTimezone($timezone);
@@ -100,6 +123,7 @@ $config = array(
                   }
 
                   echo '<a href="'.$content['url'].'" target="_blank">';
+                    echo '<div>';
                     echo $location;
                     if ($content['hints']) {
                       echo ' <sup>'.$content['hints'].'</sup>';
@@ -110,6 +134,16 @@ $config = array(
                         echo $begin->format('H:i').' Uhr &ndash; '.$end->format('H:i').' Uhr';
                       echo '</div>';
                     }
+                    echo '</div>';
+                    echo '<div>';
+                      if (isset($accessibilities[$location])) {
+                        echo '<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">';
+                          echo '<path d="M0 0h24v24H0V0z" fill="#f0d722" />';
+                          echo '<circle cx="12" cy="4" r="2" />';
+                          echo '<path d="M19 13v-2c-1.54.02-3.09-.75-4.07-1.83l-1.29-1.43c-.17-.19-.38-.34-.61-.45-.01 0-.01-.01-.02-.01H13c-.35-.2-.75-.3-1.19-.26C10.76 7.11 10 8.04 10 9.09V15c0 1.1.9 2 2 2h5v5h2v-5.5c0-1.1-.9-2-2-2h-3v-3.45c1.29 1.07 3.25 1.94 5 1.95zm-9 7c-1.66 0-3-1.34-3-3 0-1.31.84-2.41 2-2.83V12.1c-2.28.46-4 2.48-4 4.9 0 2.76 2.24 5 5 5 2.42 0 4.44-1.72 4.9-4h-2.07c-.41 1.16-1.52 2-2.83 2z" />';
+                        echo '</svg>';
+                      }
+                    echo '</div>';
                   echo '</a>';
                 }
               echo '</nav>';
@@ -117,10 +151,15 @@ $config = array(
           }
 
           ?>
-
       </section>
 
       <p>1 &ndash; Diese Teststation(en) sind <strong>ausschließlich</strong> für Mitarbeiterinnen und Mitarbeiter des jeweiligen Standortes zugelassen.</p>
+      <p>
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000">
+          <circle cx="12" cy="4" r="2" />
+          <path d="M19 13v-2c-1.54.02-3.09-.75-4.07-1.83l-1.29-1.43c-.17-.19-.38-.34-.61-.45-.01 0-.01-.01-.02-.01H13c-.35-.2-.75-.3-1.19-.26C10.76 7.11 10 8.04 10 9.09V15c0 1.1.9 2 2 2h5v5h2v-5.5c0-1.1-.9-2-2-2h-3v-3.45c1.29 1.07 3.25 1.94 5 1.95zm-9 7c-1.66 0-3-1.34-3-3 0-1.31.84-2.41 2-2.83V12.1c-2.28.46-4 2.48-4 4.9 0 2.76 2.24 5 5 5 2.42 0 4.44-1.72 4.9-4h-2.07c-.41 1.16-1.52 2-2.83 2z" />
+        </svg>
+        Diese Teststation(en) bieten einen barrierefreien Zugang.</p>
     </main>
 
     <div id="dialog-container">
